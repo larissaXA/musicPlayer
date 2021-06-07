3b0ec0a618c40ae695c86cd24466fd98604b664e
larissaXA <84881185+larissaXA@users.noreply.github.com>
2021-06-07 13:03:46 -0300

    Add files via upload
    Meu Primeiro Projeto FINALIZADO

--- musicPlayer.js
+++ musicPlayer.js
@@ -7,9 +7,29 @@ var mainBtnRight = document.getElementById('right')
 var mainBtnLeft = document.getElementById('left')
 var container = document.getElementsByClassName('container')[0]
 
+var addBtn = document.getElementById('add')
+var addDoneBtn = document.getElementById('done')
+var inputSongName = document.getElementById('inputSongName')
+var inputSongSinger = document.getElementById('inputSongSinger')
+var cancel = document.getElementById('cancel')
+
+var theme = document.getElementById('themeContainer')
+
 menuBtn.addEventListener('click',showMenu)
 mainBtnRight.addEventListener('click',passRight)
 mainBtnLeft.addEventListener('click',passLeft)
+addBtn.addEventListener('click',function(){
+    var addSongBox = document.getElementById('addSongBox')
+    addSongBox.style.display = 'flex'
+    this.style.display = 'none'
+})
+addDoneBtn.addEventListener('click',addNewSong)
+cancel.addEventListener('click',function(){
+    var addSongBox = document.getElementById('addSongBox')
+    addSongBox.style.display = 'none'
+    addBtn.style.display = 'flex'
+})
+theme.addEventListener('click',changeTheme)
 
 var aux = true
 function showMenu(){
@@ -46,9 +66,6 @@ function showMenu(){
         aux = true
     }
 }
-function opacity(){
-
-}
 
 var scrolling = 0
 var count = 0
@@ -70,3 +87,45 @@ function passLeft(){
     }
     container.scroll(scrolling,0)
 }
+
+function addNewSong(){
+    var clone = document.getElementsByClassName('songBox')[0].cloneNode(true)
+    var texts = clone.getElementsByClassName('texts')[0]
+    var songName = texts.getElementsByClassName('songName')[0]
+    var songSinger = texts.getElementsByClassName('songAuthor')[0]
+    songName.innerHTML = inputSongName.value
+    songSinger.innerHTML = inputSongSinger.value
+    var addSongBox = document.getElementById('addSongBox')
+    addSongBox.style.display = 'none'
+    addBtn.style.display = 'flex'
+    var addSong = document.getElementById('addSong')
+    container.insertBefore(clone,addSong)
+}
+
+function changeTheme(){
+    document.getElementsByTagName('body')[0].style.backgroundColor == 'turquoise' ? document.getElementsByTagName('body')[0].style.backgroundColor = '#c71585' : document.getElementsByTagName('body')[0].style.backgroundColor = 'turquoise'
+
+    document.getElementsByTagName('body')[0].style.backgroundImage == 'linear-gradient(0deg, seashell 0%, turquoise 95%)' ? document.getElementsByTagName('body')[0].style.backgroundImage = 'linear-gradient(0deg, black 0%, #c71585 95%)' : document.getElementsByTagName('body')[0].style.backgroundImage = 'linear-gradient(0deg, seashell 0%, turquoise 95%)'
+
+    Array.from(document.getElementsByClassName('texts')).forEach((value) => {
+        value.style.color == 'lightseagreen' ? value.style.color = 'magenta' : value.style.color = 'lightseagreen'
+        value.style.textShadow == '1px 1px 2px white' ? value.style.textShadow = '1px 1px 2px black' : value.style.textShadow = 'initial'
+    })
+    Array.from(document.getElementsByClassName('btnPassSong')).forEach((value) => {
+        value.classList.contains('mainBtnSeagreen') ? value.classList.remove('mainBtnSeagreen') : value.classList.add('mainBtnSeagreen')
+    })
+    document.getElementById('addSong').classList.contains('addSongSeagreen') ? document.getElementById('addSong').classList.remove('addSongSeagreen') : document.getElementById('addSong').classList.add('addSongSeagreen')
+
+    menuBtn.classList.contains('addSongSeagreen') ? menuBtn.classList.remove('addSongSeagreen') : menuBtn.classList.add('addSongSeagreen')
+
+    theme.classList.contains('addSongSeagreen') ? theme.classList.remove('addSongSeagreen') : theme.classList.add('addSongSeagreen')
+}
+
+var del = document.getElementsByClassName('delete')
+Array.from(del).forEach((value) => {
+    value.addEventListener('click',function(){
+        if(confirm('Tem certeza que deseja excluir a música de sua playlist?')){
+            value.parentElement.parentElement.parentElement.remove()
+        }
+    })
+})
